2ff5a3906537d9cd99566f1b5f9c4b5a8278ae68
tebit237 <64896943+tebit237@users.noreply.github.com>
2022-11-25 15:29:06 +0100

    forgot password

--- src/pages/ForgetPassword.jsx
+++ src/pages/ForgetPassword.jsx
@@ -1,12 +1,24 @@
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 import OAuth from "../components/OAuth";
 
 export default function ForgetPassword() {
-   // create a formData hook
+  // create a formData hook
   const [email, setEmail] = useState("");
   function onChange(e) {
     setEmail(e.target.value);
+  }
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth,email)
+      toast.success("Email was sent")
+    } catch (error) {
+      toast.error("Could not send Reset Password");
+    }
   }
   return (
     <section>
@@ -20,7 +32,7 @@ export default function ForgetPassword() {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-6">
-          <form action="">
+          <form onSubmit={onSubmit}>
             <input
               type="email"
               id="email"
